f63b603548e753a2d9e6bb136ecdd2b618fa7154
Hridoy-Chandro <hridoyrd7@gmail.com>
2024-05-28 00:19:19 +0600

    done create student feature

---
+++ src/app/modules/student/student.model.ts
@@ -0,0 +1,41 @@
+import { Schema, model } from 'mongoose';
+import { LocalGuardian, Student, UserName } from './student.interface';
+
+const userNameSchema = new Schema<UserName>({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  middleName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+});
+
+const localGuardianSchema = new Schema<LocalGuardian>({
+  name: { type: String, required: true },
+  occupation: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  address: { type: String, required: true },
+});
+
+const StudentSchema = new Schema<Student>({
+  id: { type: String },
+  name: userNameSchema,
+  gender: ['male', 'female'],
+  dateOfBirth: { type: String },
+  email: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  emergencyContactNo: { type: String, required: true },
+  bloodGroup: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+  presentAddress: { type: String, required: true },
+  permanentAddress: { type: String, required: true },
+  profileImg: { type: String },
+  isActive: ['Active', 'Blocked'],
+  localGuardian: localGuardianSchema,
+});
+
+export const StudentModel = model<Student>('Student', StudentSchema);
